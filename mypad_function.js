e8b626bbaeedc05b5088d986b46c65e957489744
Tetiana Pylypenko <155659574+QA-TetianaPylypenko@users.noreply.github.com>
2024-02-18 19:03:35 +0200

    Add comment to mypad_function.js

--- mypad_function.js
+++ mypad_function.js
@@ -1,3 +1,14 @@
+/*
+Написати власну реалізацію функцій padEnd та padStart, але у вас повинна бути одна функція pad(‘qwerty’, ‘+’, true), 
+яка приймає чотири аргументи:
+- рядок, до якого буде додаватися символ
+- символ, який буде додаватися
+- скільки додавати символів
+- булеве значення, яке вказує, куди додавати символ. Якщо true, то до початку, якщо false, то в кінець.
+Останній аргумент повинен працювати як і в оригінальній функції, а саме якщо наш рядок з 5 символів, 
+а ви вказуєте значення 6, то додаєте тільки один символ
+*/
+
 function pad(str, symbol, count, addToStart) {
   if (count <= 0) {
     return str;
@@ -14,7 +25,7 @@ function pad(str, symbol, count, addToStart) {
 }
 
 // Приклад використання
-console.log(pad("letters", "+", 2, true)); // ++letters
-console.log(pad("letters", "+", 2, false)); // letters++
-console.log(pad("letters", "+", 0, true)); // letters
-console.log(pad("letters", "+", 0, false)); // letters
+console.log(pad("qwerty", "+", 2, true)); // ++qwerty
+console.log(pad("qwerty", "+", 2, false)); // qwerty++
+console.log(pad("qwerty", "+", 0, true)); // qwerty
+console.log(pad("qwerty", "+", 0, false)); // qwerty
